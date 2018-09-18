8a23eefdf4c271506bb61656bfd5d43d94113cee
Hannah <hannahvanseeters@gmail.com>
2018-09-18 14:26:13 -0700

    task table created with 3 task instances show and index updated to incorate instances

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,20 @@
-TASKS = [
-  { id: 1, title: "Mow Lawn", description: "In the yard with the lawnmower", completion_date: "Tuesday"},
-  { id: 2, title: "Wash Dishes", description: "In the kitchen with a sponge", completion_date: "Saturday"},
-  { id: 3, title: "Dance a Jig", description: "In the yard with some friends", completion_date: "Friday"}
-]
+# TASKS = [
+#   { id: 1, title: "Mow Lawn", description: "In the yard with the lawnmower", completion_date: "Tuesday"},
+#   { id: 2, title: "Wash Dishes", description: "In the kitchen with a sponge", completion_date: "Saturday"},
+#   { id: 3, title: "Dance a Jig", description: "In the yard with some friends", completion_date: "Friday"}
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all.order(:title)
   end
 
   def show
-    id = params[:id]
-    @task = TASKS.find {|task| task[:id] == id.to_i }
+    id = params[:id].to_i
+    @task = Task.find_by(id: id)
 
     if @task.nil?
-      head :not_found
+      render :notfound, status: :not_found
     end
   end
 end
